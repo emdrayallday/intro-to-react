ad8032c492d80000b6665c8a12f0b301f7b6acf9
Mark Johnson <markjohnson4542@gmail.com>
2019-11-13 16:55:18 -0500

    now using jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,5 +4,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+const ce = React.createElement
 
+const MyTitle = function (props) {
+    return (
+        <div><h1 style={{ color: props.color }}>{props.title}</h1></div>
+    )
+}
 
+const MyFirstComponent = function () {
+    return (
+        <div>
+            <MyTitle title="Game of Thrones" color="YellowGreen" />
+            <MyTitle title="Stranger Things" color="peru" />
+            <MyTitle title="Rick and Morty" color="blue" />
+        </div>
+    )
+}
+
+render(ce(<MyFirstComponent />), document.getElementById('app'));
