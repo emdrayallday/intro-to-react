14454cc09188d7ebe56f4ab8166cda713f14be81
Mark Johnson <markjohnson4542@gmail.com>
2019-11-13 16:31:57 -0500

    babel config

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ Apply styles to file with --write
 ```
 prettier --single-quote --print-width-120 --write ClientApp.jsx
 ```
+
+# Webpack
+
+At it's simplest, it packages several files to one file (combining and minifying)
+
+# Babel
+
+Transforms jsx and es6 to to plain javascript or vanilla js. plus much other things
--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,8 @@
-const MyTitle = function (props) {
-    return (
-        React.createElement('div', null,
-            React.createElement('h1', { style: { color: props.color } }, props.title)
-        )
-    )
-}
+// Import is statically used so it can be used by our tools for tree shaking
+// Only include code that will be ran
 
-// The MyFirstComponent is a class, the createElement is just creating a div
-const MyFirstCompoonent = function () {
-    return React.createElement('div', null,
-        React.createElement(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-        React.createElement(MyTitle, { title: 'asdf', color: 'GreenYellow' }),
-        React.createElement(MyTitle, { title: 'asdfasdf', color: 'LimeGreen' }),
-        React.createElement(MyTitle, { title: 'rhy', color: 'peru' })
-    )
-}
-// In here, the createElement is creating an INSTANCE of MyFirstComponent
-ReactDOM.render(
-    React.createElement(MyFirstCompoonent),
-    document.getElementById('app')
-)
+import React from 'react'
+import { render } from 'react-dom'
 
-// This means that you can create components of other components
+
+
